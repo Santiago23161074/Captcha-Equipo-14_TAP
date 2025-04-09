f7eb00e91fdd8bec6425c613927db272f413a171
Goyo0905 <santiagoenmanuelp@gmail.com>
2025-04-09 09:36:38 -0600

    Delete Docs/javadoc directory

--- Docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Paquete_Captcha","l":"Captcha"},{"p":"Paquete_Captcha","l":"Validacion_Captcha"}];updateSearchResults();
